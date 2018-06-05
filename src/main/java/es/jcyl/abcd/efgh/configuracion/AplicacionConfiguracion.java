a3b3ad0d9cf1ef9bf227a2fae69c4a47f33cad79
blasillo <blasillous@gmail.com>
2018-06-05 20:37:24 +0200

    Revisado [master]

--- src/main/java/es/jcyl/abcd/efgh/configuracion/AplicacionConfiguracion.java
+++ src/main/java/es/jcyl/abcd/efgh/configuracion/AplicacionConfiguracion.java
@@ -1,9 +1,5 @@
 package es.jcyl.abcd.efgh.configuracion;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
